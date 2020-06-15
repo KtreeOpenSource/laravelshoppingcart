c3af0a7a7a1c752edbed57ef7d92981901569a86
sravya.vemulapally <sravya.vemulapally@ktree.us>
2020-06-15 19:17:40 +0530

    darryldecode-cart changes for Bhaswa project

--- src/Darryldecode/Cart/Cart.php
+++ src/Darryldecode/Cart/Cart.php
@@ -34,6 +34,13 @@ class Cart
     protected $instanceName;
 
     /**
+     * the session key use for the cart
+     *
+     * @var
+     */
+    protected $sessionKey;
+
+    /**
      * the session key use to persist cart items
      *
      * @var
@@ -68,10 +75,29 @@ class Cart
         $this->events = $events;
         $this->session = $session;
         $this->instanceName = $instanceName;
-        $this->sessionKeyCartItems = $session_key . '_cart_items';
-        $this->sessionKeyCartConditions = $session_key . '_cart_conditions';
-        $this->fireEvent('created');
+        $this->sessionKey = $session_key;
+        $this->sessionKeyCartItems = $this->sessionKey . '_cart_items';
+        $this->sessionKeyCartConditions = $this->sessionKey . '_cart_conditions';
         $this->config = $config;
+        $this->fireEvent('created');
+    }
+
+    /**
+     * sets the session key
+     *
+     * @param string $sessionKey the session key or identifier
+     * @return $this|bool
+     * @throws \Exception
+     */
+    public function session($sessionKey)
+    {
+        if(!$sessionKey) throw new \Exception("Session key is required.");
+
+        $this->sessionKey = $sessionKey;
+        $this->sessionKeyCartItems = $this->sessionKey . '_cart_items';
+        $this->sessionKeyCartConditions = $this->sessionKey . '_cart_conditions';
+
+        return $this;
     }
 
     /**
@@ -508,6 +534,22 @@ class Cart
         );
     }
 
+    /**
+     * get cart sub total without conditions
+     * @param bool $formatted
+     * @return float
+     */
+    public function getSubTotalWithoutConditions($formatted = true)
+    {
+        $cart = $this->getContent();
+
+        $sum = $cart->sum(function ($item) {
+            return $item->getPriceSum();
+        });
+
+        return Helpers::formatValue(floatval($sum), $formatted, $this->config);
+    }    
+    
     /**
      * get cart sub total
      * @param bool $formatted
